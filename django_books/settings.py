e6beaf80a19d095eeacc1f83710cdb3644ee1902
seba <sebas.a.nunez@gmail.com>
2020-11-10 21:15:33 -0300

    add config for heroku

--- django_books/settings.py
+++ django_books/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+django_heroku.settings(locals())
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
